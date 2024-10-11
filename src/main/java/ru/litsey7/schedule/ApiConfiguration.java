cda933248e7a4e41654b7fd0e22e323e6e09bd3c
Karim <kfmust@gmail.com>
2024-10-11 20:11:14 +0300

    Api Config Created

---
+++ src/main/java/ru/litsey7/schedule/ApiConfiguration.java
@@ -0,0 +1,20 @@
+package ru.litsey7.schedule;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// Конфиг спринг приложения
+@Configuration
+public class ApiConfiguration implements WebMvcConfigurer {
+    // Обработка CORS
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}

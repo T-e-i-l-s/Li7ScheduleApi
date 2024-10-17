68982aac1cdeb4d93dd3799b29514ab9e30b72d0
Karim <kfmust@gmail.com>
2024-10-17 23:45:14 +0300

    Schedule Post Request Init

--- src/main/java/ru/litsey7/schedule/controllers/LessonScheduleController.java
+++ src/main/java/ru/litsey7/schedule/controllers/LessonScheduleController.java
@@ -2,15 +2,16 @@ package ru.litsey7.schedule.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ru.litsey7.schedule.data.repositories.LessonScheduleRepository;
 import ru.litsey7.schedule.data.source.database.entities.LessonScheduleEntity;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 // Эндпоинт для работы с расписанием дня
 @RestController
@@ -41,5 +42,11 @@ public class LessonScheduleController {
                 .peek(LessonScheduleEntity::getLessonsAsJson)
                 .toList()
                 .get(0);
+    }
+
+    // Post запрос к /api/full_lesson_schedule
+    @PostMapping("/full_lesson_schedule")
+    public ResponseEntity<String> setSchedule(@RequestBody List<LessonScheduleEntity> schedule) {
+        return ResponseEntity.ok("Processed");
     }
 }
